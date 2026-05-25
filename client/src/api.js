d28f7c9e6e485a91b60033e421ab34d581692ba9
Gandhi Tech AI <kumarayshu852@gmail.com>
2026-05-25 15:40:27 +0530

    Update api.js
    change url

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import  axios from 'axios';
 
 const API =axios.create({
-    baseURL: https://loan-management-system-kpvw.onrender.com
+    baseURL: "https://loan-management-system-kpvw.onrender.com"
 });
 
 // har request mein token automatically lagao
